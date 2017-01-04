f037433597922291ec7275769c597c90cde86bcb
Ayrat Badykov <ayratin555@gmail.com>
2017-01-04 22:15:30 +0300

    add proximity matrix calculation

--- lib/masters_work.ex
+++ lib/masters_work.ex
@@ -1,2 +1,11 @@
 defmodule MastersWork do
+  @files [
+         "../data/phonetics_vowels.csv",
+         "../data/phonetics_consonantal.csv"
+       ]
+
+  def execute do
+    {communities, column_names, values} = Preprocessor.read(@files)
+    Measure.proximity_matrix(values)
+  end
 end
